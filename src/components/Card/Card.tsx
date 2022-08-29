5fdd8c599226bf9fe377e495bf1e568a8f8a40a3
Rodolfo Dominguez <roddom67@gmail.com>
2022-08-29 12:00:03 -0300

    Ajustes de diseño

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import './Card.scss'
+import React from "react";
+// import { faHome } from "@fontawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fontawesome/react-fontawesome";
+import "./Card.scss";
 
+const Card = ({ image }: { image: string}) => {
+	return (
+		<div className="col-2" >
+			<div className="card">
+        <div className="card-image">
+          <img src={`${image}`} alt="imagenPelicula" />
+          <span className="raiting">
+            <i className="fa-solid fa-clapperboard"></i>
+            <span>80%</span>
+          </span>
+        </div>
+        <div className="card-content">
+          <h4>Titulo <span>(2002)</span></h4>
+        </div>
+			</div>
+		</div>
+	);
+};
 
-const Card = ( { image }:{ image:string }) => {  
-  
-  return (
-    <div className='card'>
-        <img src={`${image}`} alt="imagenPelicula" />
-    </div>
-  )
-}
-
-export default Card
+export default Card;
--- src/containers/CardContainer/CardContainer.tsx
+++ src/containers/CardContainer/CardContainer.tsx
@@ -1,35 +1,33 @@
-import React , { useContext, useState } from 'react'
-import Card from '../../components/Card/Card'
-import { Context } from '../../context/Context'
+import React, { useContext, useState } from "react";
+import Card from "../../components/Card/Card";
+import { Context } from "../../context/Context";
 
-import './CardContainer.scss'
+import "./CardContainer.scss";
 
 const CardContainer = () => {
+	const { setQuery, data, imageUrl } = useContext(Context);
+	const [paginate, setPaginate] = useState(12);
 
-  const { setQuery, data, imageUrl } = useContext(Context)  
-  const [ paginate, setPaginate ] = useState(12)  
+	const handleQuerySearch = (e: any) => {
+		setQuery(e.target.value);
+	};
+	const handlePaginate = () => {
+		setPaginate((prev) => prev + 12);
+	};
 
-  const handleQuerySearch = (e:any) => {
-    setQuery(e.target.value)    
-  }
-  const handlePaginate = () => {
-    setPaginate( prev => prev + 12 )
-  }
+	return (
+		<div className="container">
+			<input type="search" onChange={(e) => handleQuerySearch(e)} />
 
-  return (
-    <>
-    <input type="text" onChange={(e)=>handleQuerySearch(e)}/>
-    <div className='cardContainer'>
-      {
-        data?.slice(0, paginate).map( (film:any) => {
-          let { poster_path } = film;
-          return <Card image={`${imageUrl+poster_path}`}/>
-        })
-      }
-    </div>
-    <button onClick={handlePaginate}>Más títulos</button>
-    </>
-  )
-}
+			<div className="cardContainer row">
+				{data?.slice(0, paginate).map((film: any) => {
+					let { poster_path } = film;
+					return <Card image={`${imageUrl + poster_path}`} key={film.id} />;
+				})}
+			</div>
+			<button onClick={handlePaginate}>Más títulos</button>
+		</div>
+	);
+};
 
-export default CardContainer
+export default CardContainer;
